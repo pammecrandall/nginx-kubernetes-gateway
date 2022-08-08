eccc26a27610ae9cd82431acf1f2282e81bd10fa
Michael Pleshakov <michael@nginx.com>
2022-08-08 09:50:55 -0600

    Ensure the handler terminates

--- internal/events/loop.go
+++ internal/events/loop.go
@@ -53,25 +53,36 @@ func (el *EventLoop) Start(ctx context.Context) error {
 	// We will use this cond to signal the handler goroutine to handle the current batch.
 	cond := sync.NewCond(&el.lock)
 
+	isShuttingDown := func() bool {
+		select {
+		case <-ctx.Done():
+			return true
+		default:
+			return false
+		}
+	}
+
+	// The handler goroutine will use this channel to signal its termination.
+	handlerTerminated := make(chan struct{})
+
 	// This is the handler goroutine.
 	// It will handle the current batch once it has at least one event.
-	// It will run until the ctx is canceled.
+	// It will run until the eventLoop is shutting down.
 	go func() {
 		for {
 			cond.L.Lock()
 
-			// If the current batch has zero events, sleep until awaken
-			for len(el.batch) == 0 {
-				// Terminate the goroutine if the context is canceled.
-				select {
-				case <-ctx.Done():
-					cond.L.Unlock()
-					return
-				default:
-				}
-
-				// Sleep and wait to get awaken.
+			// If the current batch has zero events and the eventLoop isn't shutting down, sleep until awaken.
+			for len(el.batch) == 0 && !isShuttingDown() {
 				cond.Wait()
+			}
+
+			if isShuttingDown() {
+				// Terminate the goroutine if the eventLoop is shutting down.
+				cond.L.Unlock()
+
+				close(handlerTerminated)
+				return
 			}
 
 			oldBatch := el.batch
@@ -88,8 +99,20 @@ func (el *EventLoop) Start(ctx context.Context) error {
 	for {
 		select {
 		case <-ctx.Done():
-			// Signal the handler goroutine so that it terminates.
+			// Locking here is needed to make sure the signal is only sent either when the handler goroutine is
+			// sleeping or when it is handling the previous batch before acquiring the lock again.
+			// This way we know the signal will not get lost.
+			cond.L.Lock()
+
+			// Signal the handler goroutine so that it terminates. Two possible outcomes:
+			// - If the handler is sleeping, it will wake up and terminate.
+			// - If the handler is busy handling the previous batch, it will continue handling it. Once it is done, it
+			// will terminate.
 			cond.Signal()
+			cond.L.Unlock()
+
+			// Wait until the handler goroutine terminates.
+			<-handlerTerminated
 
 			// although Start() always returns nil as an error, we cannot remove the error from the returned parameters,
 			// because "sigs.k8s.io/controller-runtime/pkg/manager".Runnable requires it, which Start() must satisfy.
